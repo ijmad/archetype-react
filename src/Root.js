3c12365addbb3fe52538dd1d93d048ce4cc25626
Ian Maddison <ian@i.need.stronger.coffee>
2021-10-27 18:56:08 +0100

    Add styled-components, GlobalStyle

--- src/Root.js
+++ src/Root.js
@@ -2,10 +2,12 @@ import React, { StrictMode } from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
 
+import GlobalStyle from "./GlobalStyle";
 import App from "./App";
 
 ReactDOM.render(
   <StrictMode>
+    <GlobalStyle />
     <Router>
       <App />
     </Router>
--- webpack/common.js
+++ webpack/common.js
@@ -20,7 +20,7 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: paths.public + "/index.html",
+      template: `${paths.public}/index.html`,
     }),
   ],
 };
